5f7b3b139353035fea1070c69665ab55a6f2f2c5
Md Ayaz <127990006+ayaz80450@users.noreply.github.com>
2024-09-30 11:09:04 +0530

    Update Services.jsx

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -17,9 +17,6 @@ const Services = () => {
              <h3>{service.s_no}</h3>
              <h2>{service.s_name}</h2>
              <p>{service.s_desc}</p>
-             <div className='services-readmore'>
-              <p>Read More</p>
-              <img src={arrow_icon} alt="" />
              </div>
           </div>
         })}
